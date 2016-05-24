fa1c36ab9e1f6639ee5615ba835fe645455c87db
thepangman <azzapang40@yahoo.com>
2016-05-25 01:54:42 +1200

    Midway through probability table

--- src/PartOne.java
+++ src/PartOne.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 
 public class PartOne {
 	static int[][] labelled = new int[200][13];
-	static int[][] probabilityTable = new int [200][13];
-	int[][] unlabelled = new int[12][10];
+	static int[][] probabilityTable = new int [13][2];
+	int[][] unlabelled = new int[25][10];
 
 	public static void main(String[] args) throws IOException {
-		readData("spamLabelled.dat");
+		readLabelledData("spamLabelled.dat");
 		printData();
 		calcProbabilities();
 	}
 
-	public static void readData(String fileName) throws IOException {
+	public static void readLabelledData(String fileName) throws IOException {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(fileName));
 			int lineCount = 0;
@@ -52,7 +52,145 @@ public class PartOne {
 	}
 	
 	public static void calcProbabilities(){
+		int totalSpam = 1; //Small constant to account for zero counts
+		int totalNotSpam = 1;
+		int totalA1 = 1;
+		int totalNotA1 = 1;
+		int totalA2 = 1;
+		int totalNotA2 = 1;
+		int totalA3 = 1;
+		int totalNotA3 = 1;
+		int totalA4 = 1;
+		int totalNotA4 = 1;
+		int totalA5 = 1;
+		int totalNotA5 = 1;
+		int totalA6 = 1;
+		int totalNotA6 = 1;
+		int totalA7 = 1;
+		int totalNotA7 = 1;
+		int totalA8 = 1;
+		int totalNotA8 = 1;
+		int totalA9 = 1;
+		int totalNotA9 = 1;
+		int totalA10 = 1;
+		int totalNotA10 = 1;
+		int totalA11 = 1;
+		int totalNotA11 = 1;
+		int totalA12 = 1;
+		int totalNotA12 = 1;
 		
+		for(int x = 0;x<labelled.length;x++){
+			if(labelled[x][12]==1){
+				totalSpam++;
+			}
+			else if(labelled[x][12]==0){
+				totalNotSpam++;
+			}
+		}
+		System.out.println("Total Spam is: " + totalSpam);
+		System.out.println("Total notSpam is: " + totalNotSpam);
+		probabilityTable[0][0] = totalSpam/200; //P(Class) = Spam
+		probabilityTable[0][1] = totalNotSpam/200; //P(Class) = Not Spam
+		for(int x = 0;x<labelled.length;x++){
+			if(labelled[x][1]==1){
+				totalA1++;
+			}
+			else if(labelled[x][1]==0){
+				totalNotA1++;
+			}
+		}
+		int spamGivenA1 = totalA1/totalSpam;
+		int notSpamGivenA1 = totalA1/totalNotSpam;
+		int spamGivenNotA1 = totalNotA1/totalSpam;
+		int notSpamGivenNotA1 = totalNotA1/totalNotSpam;
+		probabilityTable[1][0] = spamGivenA1; //P(Attribute 1 | Class) = Spam
+		probabilityTable[1][1] = notSpamGivenA1; //P(Attribute 1 | Class) = Not Spam
+		probabilityTable[2][0] = spamGivenNotA1; //P(Not Attribute 1 | Class) = Spam
+		probabilityTable[2][1] = notSpamGivenNotA1; //P(Not Attribute 1 | Class) = Not Spam
+		for(int x = 0;x<labelled.length;x++){
+			if(labelled[x][2]==1){
+				totalA2++;
+			}
+			else if(labelled[x][2]==0){
+				totalNotA2++;
+			}
+		}
+		int spamGivenA2 = totalA2/totalSpam;
+		int notSpamGivenA2 = totalA2/totalNotSpam;
+		int spamGivenNotA2 = totalNotA2/totalSpam;
+		int notSpamGivenNotA2 = totalNotA2/totalNotSpam;
+		probabilityTable[3][0] = spamGivenA2; //P(Attribute 2 | Class) = Spam
+		probabilityTable[3][1] = notSpamGivenA2; //P(Attribute 2 | Class) = Not Spam
+		probabilityTable[4][0] = spamGivenNotA2; //P(Not Attribute 2 | Class) = Spam
+		probabilityTable[4][1] = notSpamGivenNotA2; //P(Not Attribute 2 | Class) = Not Spam
+
+		for(int x = 0;x<labelled.length;x++){
+			if(labelled[x][3]==1){
+				totalA3++;
+			}
+			else if(labelled[x][3]==0){
+				totalNotA3++;
+			}
+		}
+		int spamGivenA3 = totalA3/totalSpam;
+		int notSpamGivenA3 = totalA3/totalNotSpam;
+		int spamGivenNotA3 = totalNotA3/totalSpam;
+		int notSpamGivenNotA3 = totalNotA3/totalNotSpam;
+		probabilityTable[5][0] = spamGivenA3; //P(Attribute 3 | Class) = Spam
+		probabilityTable[5][1] = notSpamGivenA3; //P(Attribute 3 | Class) = Not Spam
+		probabilityTable[6][0] = spamGivenNotA3; //P(Not Attribute 3 | Class) = Spam
+		probabilityTable[6][1] = notSpamGivenNotA3; //P(Not Attribute 3 | Class) = Not Spam
+
+		for(int x = 0;x<labelled.length;x++){
+			if(labelled[x][4]==1){
+				totalA4++;
+			}
+			else if(labelled[x][4]==0){
+				totalNotA4++;
+			}
+		}
+		int spamGivenA4 = totalA4/totalSpam;
+		int notSpamGivenA4 = totalA4/totalNotSpam;
+		int spamGivenNotA4 = totalNotA4/totalSpam;
+		int notSpamGivenNotA4 = totalNotA4/totalNotSpam;
+		probabilityTable[7][0] = spamGivenA4; //P(Attribute 4 | Class) = Spam
+		probabilityTable[7][1] = notSpamGivenA4; //P(Attribute 4 | Class) = Not Spam
+		probabilityTable[8][0] = spamGivenNotA4; //P(Not Attribute 4 | Class) = Spam
+		probabilityTable[8][1] = notSpamGivenNotA4; //P(Not Attribute 4 | Class) = Not Spam
+
+		for(int x = 0;x<labelled.length;x++){
+			if(labelled[x][5]==1){
+				totalA5++;
+			}
+			else if(labelled[x][5]==0){
+				totalNotA5++;
+			}
+		}
+		int spamGivenA5 = totalA5/totalSpam;
+		int notSpamGivenA5 = totalA5/totalNotSpam;
+		int spamGivenNotA5 = totalNotA5/totalSpam;
+		int notSpamGivenNotA5 = totalNotA5/totalNotSpam;
+		probabilityTable[9][0] = spamGivenA5; //P(Attribute 4 | Class) = Spam
+		probabilityTable[9][1] = notSpamGivenA5; //P(Attribute 4 | Class) = Not Spam
+		probabilityTable[10][0] = spamGivenNotA5; //P(Not Attribute 4 | Class) = Spam
+		probabilityTable[10][1] = notSpamGivenNotA5; //P(Not Attribute 4 | Class) = Not Spam
+
+		for(int x = 0;x<labelled.length;x++){
+			if(labelled[x][6]==1){
+				totalA6++;
+			}
+			else if(labelled[x][6]==0){
+				totalNotA6++;
+			}
+		}
+		int spamGivenA6 = totalA6/totalSpam;
+		int notSpamGivenA6 = totalA6/totalNotSpam;
+		int spamGivenNotA6 = totalNotA6/totalSpam;
+		int notSpamGivenNotA6 = totalNotA6/totalNotSpam;
+		probabilityTable[11][0] = spamGivenA6; //P(Attribute 4 | Class) = Spam
+		probabilityTable[11][1] = notSpamGivenA6; //P(Attribute 4 | Class) = Not Spam
+		probabilityTable[12][0] = spamGivenNotA6; //P(Not Attribute 4 | Class) = Spam
+		probabilityTable[12][1] = notSpamGivenNotA6; //P(Not Attribute 4 | Class) = Not Spam
 	}
 
 }
